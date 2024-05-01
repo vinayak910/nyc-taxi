4c83f2dd50678d5eb078a4f94a97434f2278d602
Vinayak Chhabra <vinayakchhabra545.vc@gmail.com>
2024-05-01 07:41:51 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine learning project to predict trip duration for taxis in nyc',
+    author='Vinayak',
+    license='MIT',
+)
