aa54c325f2943fc3a1cc8110ff7422e9f2c8e4e3
Vinayak Chhabra <vinayakchhabra545.vc@gmail.com>
2024-05-08 19:45:49 +0530

    Added code for extracting zip files

--- src/data/extract_dataset.py
+++ src/data/extract_dataset.py
@@ -1,44 +1,48 @@
 import logging
 from zipfile import ZipFile
 from pathlib import Path
-from src.logger import create_log_path , CustomLogger
+from src.logger import create_log_path, CustomLogger
 
 # path to save the log files
 log_file_path = create_log_path('extract_dataset')
 
-# create the custom logger object
-extract_logger = CustomLogger(logger_name='extract_dataset',
-                              log_filename=log_file_path)
+# create the cutom Logger object
+extract_logger = CustomLogger('extract_dataset' , log_file_path)
+
 # set the level of logging to INFO
-extract_logger.set_log_level(level=logging.INFO)
+extract_logger.set_log_level(level = logging.INFO)
 
 
-def extract_zip_file(input_path : Path , output_path :Path):
+def extract_zip_file(input_path:Path , output_path : Path):
     with ZipFile(file = input_path) as f:
         f.extractall(path = output_path)
         input_file_name = input_path.stem + input_path.suffix
         extract_logger.save_logs(msg = f'{input_file_name} extracted successfully at the target path',
                                  log_level = 'info')
         
-
 def main():
-    #current file path
-    current_path = Path(__file__)  # extract_dataset.py path
+    # current file path
+    current_path = Path(__file__)
     #root directory path
     root_path = current_path.parent.parent.parent
-    #raw data directory path
+    # raw data directory path
     raw_data_path = root_path/'data'/'raw'
-    # output path for the zip files
-    output_path = raw_data_path / 'extracted'
-    # make the directory for the path
-    output_path.mkdir(parents=True,exist_ok=True)
+    # zip files path
     input_path = raw_data_path/'zipped'
+    # making directory for extracted data
+    output_path = raw_data_path/'extracted'
+    output_path.mkdir(exist_ok=True , parents=True)
+
 
     # extract the train and test files
     # for the train file
-    extract_zip_file(input_path= input_path / 'train.zip',
-                     output_path= output_path )
+    extract_zip_file(input_path= input_path/'train.zip' , output_path= output_path)
     # for the test file
-    extract_zip_file(input_path= input_path / 'test.zip',
-                     output_path= output_path)
-                     
+    extract_zip_file(input_path= input_path/'test.zip', output_path= output_path)
+    
+
+
+if __name__ == "__main__":
+    # call the main function
+    main()
+    
--- src/logger.py
+++ src/logger.py
@@ -1,122 +1,118 @@
-import logging
+import logging 
 from pathlib import Path
-import datetime as dt
+import datetime as dt 
 
 
 
-
-
-def create_log_path(module_name: str) -> str:
+def create_log_path(module_name:str)-> str:
     """
-    Create a log file path based on the current date and the provided module_name.
+    Create a log file path based on current date and the provided module name
 
     Parameters:
-    - module_name (str): The name of the log file.
+    - module_name(str) : The name of the log file
 
     Returns:
-    - str: The complete path of the log file.
+    - str: The complete path of the log file
     """
+
     current_date = dt.date.today()
-    # create a logs folder in the root directory
+    #create a logs folder in the root directory
     root_path = Path(__file__).parent.parent
-    # create path for logs folder
-    log_dir_path = root_path / 'logs'
-    log_dir_path.mkdir(exist_ok=True)
-    
-    # create folder for a specific module
-    module_log_path = log_dir_path / module_name
-    module_log_path.mkdir(exist_ok=True,parents=True)
-    # convert the date to str
-    current_date_str = current_date.strftime("%d-%m-%Y")  # ! error at this point if not fixed
+    #create path for logs folder
+    log_dir_path = root_path /'logs'
+    log_dir_path.mkdir(exist_ok = True)
+
+
+    #create a folder for specific module
+    module_path_log = log_dir_path/ module_name
+    module_path_log.mkdir(exist_ok=True , parents = True)
+
+    # convert the current date into string
+    current_date_str = current_date.strftime("%d-%m-%Y")
     # create log files based on current date
-    log_file_name = module_log_path / (current_date_str + '.log')
+    log_file_name = module_path_log/(current_date_str + '.log')
     return log_file_name
 
 
 class CustomLogger:
-    def __init__(self,logger_name,log_filename):
+    def __init__(self , logger_name , log_filename):
         """
         Initializes a custom logger with the specified name and log file.
 
         Parameters:
-        - logger_name (str): Name of the logger.
-        - log_filename (str): Path to the log file.
+        - logger_name(str) : Name of the logger.
+        - log_filename(str) : Path to the log file. 
         """
-        self.__logger = logging.getLogger(name=logger_name)
+
+        self.__logger = logging.getLogger(name = logger_name)
         self.__log_path = log_filename
-        
+
         # make the file handler object
-        file_handler = logging.FileHandler(filename=self.__log_path,
-                                           mode='a')
+        file_handler = logging.FileHandler(filename= self.__log_path , mode = 'a')
+
         # add file handler to logger
         self.__logger.addHandler(hdlr=file_handler)
         # formatter for logs
         log_format = "%(asctime)s - %(levelname)s : %(message)s"
         time_format = '%d-%m-%Y %H:%M:%S'
-        formatter = logging.Formatter(fmt=log_format,
-                                      datefmt=time_format)
-        # add formatter to the file handler
-        file_handler.setFormatter(fmt=formatter)
-        
-        
+        formatter = logging.Formatter(fmt = log_format , datefmt=time_format)
+
+        # add formatter to file handler
+        file_handler.setFormatter(fmt = formatter)
+
     def get_log_path(self):
         """
-        Returns the path to the log file.
+        Returns the path to the log file
 
         Returns:
-        - str: Log file path.
+        - str: Log file path 
         """
         return self.__log_path
-        
+    
     def get_logger(self):
         """
         Returns the logger object.
 
         Returns:
-        - logging.Logger: Logger object.
+        - logging.logger: Logger object
         """
-        return self.__logger    
-        
-    def set_log_level(self,level=logging.DEBUG):
+        return self.__logger
+    
+    def set_log_level(self , level = logging.DEBUG):
         """
         Sets the log level for the logger.
 
         Parameters:
-        - level (int): Logging level (e.g., logging.DEBUG, logging.INFO).
+        - level(int) : Logging level (e.g. -> logging.DEBUG , logging.INFO).
         """
         logger = self.get_logger()
-        logger.setLevel(level=level)
-        
-    def save_logs(self,msg,log_level='info'):
+        logger.setLevel(level = level)
+    
+    def save_logs(self , msg , log_level = 'info'):
         """
-        Saves logs to the specified log file with the given message and log level.
+        Saves logs to the specific log file with the given message and log level.
 
         Parameters:
         - msg (str): Log message.
-        - log_level (str): Log level ('debug', 'info', 'warning', 'error', 'exception', 'critical').
+        - log_level (str): Log level('debug' , 'info' , 'warning' , 'error' , 'exception', 'critical').
         """
-        # get the logger
-        logger = self.get_logger()
-        # save the logs to the file using the given message
-        if log_level == 'debug':
-            logger.debug(msg=msg)
-        elif log_level == 'info':
-            logger.info(msg=msg)
-        elif log_level == 'warning':
-            logger.warning(msg=msg)
-        elif log_level == 'error':
-            logger.error(msg=msg)
-        elif log_level == 'exception':
-            logger.exception(msg=msg)
-        elif log_level == 'critical':
-            logger.critical(msg=msg)
         
+        logger = self.get_logger()
+        # save the logs to the file using given message
+        if log_level == 'debug':
+            logger.debug(msg = msg)
+        elif log_level == 'info':
+            logger.info(msg = msg)
+        elif log_level == 'warning':
+            logger.warning(msg = msg)
+        elif log_level == 'error':
+            logger.error(msg = msg)
+        elif log_level == 'exception':
+            logger.exception(msg = msg)
+        elif log_level == 'critical':
+            logger.critical(msg = msg)
 
 if __name__ == "__main__":
-
-    logger = CustomLogger(logger_name='my_logger',
-                          log_filename=create_log_path('test'))
-    
+    logger = CustomLogger(logger_name= 'my_logger' , log_filename = create_log_path('test') )
     logger.set_log_level()
-    
-    logger.save_logs('save me code is breaking',log_level='critical')
+    logger.save_logs('save me code is breaking' , log_level='critical')
